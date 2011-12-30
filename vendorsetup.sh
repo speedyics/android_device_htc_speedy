956e6e9e094e0405b927213787d8564ec87e4e93
speedyics <jhuymaier@gmail.com>
2011-12-30 00:54:25 -0500

    Enable Default Lunch Combo for more generic device overlay

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,6 +20,5 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-#We're only going to be building Evervolv, so comment this out for now
 #add_lunch_combo htc_speedy-userdebug
-#add_lunch_combo htc_speedy-eng
+add_lunch_combo htc_speedy-eng
